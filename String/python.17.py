a474ac8d6663379abd819c75200aea5dbfd4d9f1
7SNEHA <100269735+7SNEHA@users.noreply.github.com>
2022-03-24 12:23:02 +0530

    solution17

---
+++ String/python.17.py
@@ -0,0 +1,11 @@
+a=input("Enter any string : ")
+l=list(a.split ())
+c=0
+l1=[]
+for i in l:
+    b=i[::-1]
+    if i==b:
+        l1.append(i)
+        print (i)
+        c=c+1
+print("Total {} palindrom word in string {}".format(c,l1))
